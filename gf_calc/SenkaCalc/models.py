80a94ed12fdeef952272a50c82c38fa7bdefc25e
kou-000 <p231067k@mail.kyutech.jp>
2019-10-10 00:27:16 +0900

    Create Senka table in DataBase.

--- gf_calc/SenkaCalc/models.py
+++ gf_calc/SenkaCalc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Senka(models.Model):
+    # senkaId : Integer型で主キー
+    senkaId = models.IntegerField(primary_key=True)
+    # maxBoxNum : Integer型
+    maxBoxNum = models.IntegerField()
+    # minBoxNum : Integer型
+    minBoxNum = models.IntegerField()
+    # senkaNum : Integer型
+    senkaNum = models.IntegerField()
--- gf_calc/gf_calc/settings.py
+++ gf_calc/gf_calc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SenkaCalc.apps.SenkacalcConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
