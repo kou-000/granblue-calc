1025ef9dfcd012825ce9d5f36c64291fb87d19d5
kou-000 <p231067k@mail.kyutech.jp>
2019-10-15 00:11:46 +0900

    create senka database

--- gf_calc/SenkaCalc/models.py
+++ gf_calc/SenkaCalc/models.py
@@ -5,8 +5,11 @@ class Senka(models.Model):
     # senkaId : Integer型で主キー
     senkaId = models.IntegerField(primary_key=True)
     # maxBoxNum : Integer型
-    maxBoxNum = models.IntegerField()
+    maxBoxNum = models.IntegerField(null=True, blank=True)
     # minBoxNum : Integer型
     minBoxNum = models.IntegerField()
     # senkaNum : Integer型
     senkaNum = models.IntegerField()
+
+    def __str__(self):
+        return str(self.senkaId) + ', ' + str(self.maxBoxNum) + ', ' + str(self.minBoxNum) + ', ' + str(self.senkaNum)
